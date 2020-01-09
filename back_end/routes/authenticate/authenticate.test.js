8ba9a8c70fd31d2f51f15dab2ab974d5173de96a
Joshua G Evans <jevan5@uwo.ca>
2020-01-09 10:41:32 -0500

    Add unit tests for routes in backend
    Added unit tests for the following routes in the backend:
    - /users
    - /authenticate

--- back_end/routes/authenticate/authenticate.test.js
+++ back_end/routes/authenticate/authenticate.test.js
@@ -3,7 +3,7 @@ const chaiHttp      = require('chai-http');
 const mongoose      = require('mongoose');
 const app           = require('../../server');
 const environment   = require('../../environment');
-const User          = require('../../models/user');
+const User          = require('../../models/user/user');
 const cryptoHelper  = require('../../tools/cryptoHelper');
 
 chai.use(chaiHttp);
